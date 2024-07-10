e9cb402e2b373bac547051fab8bf851d0fc1d29b
GermanMerlo <germanmerlodiaz@gmail.com>
2024-07-10 22:14:40 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const who = ["The dog", "My grandma", "The mailman", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "my phone", "the car"];
+const when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+const excusaText = document.getElementById("excuse");
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = getExcuse();
+  excusaText.textContent = excuse;
 };
+
+function getExcuse() {
+  const whoIndex = Math.floor(Math.random() * who.length);
+  const actionIndex = Math.floor(Math.random() * action.length);
+  const whatIndex = Math.floor(Math.random() * what.length);
+  const whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
